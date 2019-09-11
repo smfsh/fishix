a1303f21e9acb7d28fec5fbc38109310eb22de21
Miles McLean <Mills00013@gmail.com>
2019-09-11 09:17:27 -0500

    Added output text to loader

--- boot_sect_simple.asm
+++ boot_sect_simple.asm
@@ -1,6 +1,20 @@
-; Infinite loop (e9 fd ff)
-loop:
-    jmp loop
+mov ah, 0x0e ; teletype mode
+mov al, 'H' ; send ASCII Hex "H" into al register
+int 0x10 ; call screen print interrupt
+
+mov al, 'E' ; send ASCII Hex "H" into al register
+int 0x10 ; call screen print interrupt
+
+mov al, 'L' ; send ASCII Hex "H" into al register
+int 0x10 ; call screen print interrupt
+
+mov al, 'L' ; send ASCII Hex "H" into al register
+int 0x10 ; call screen print interrupt
+
+mov al, 'O' ; send ASCII Hex "H" into al register
+int 0x10 ; call screen print interrupt
+
+jmp $ ; jump to current execution pointer, effectively an infinite loop
 
 ; Fill with 510 zeros minus the size of the previous code
 times 510-($-$$) db 0
