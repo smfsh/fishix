c3f275e6ce03aed304e9aad1f0b010b5be62f089
Miles McLean <Mills00013@gmail.com>
2019-09-12 15:31:31 -0500

    Added hex printing for error messages on disk

--- storage.asm
+++ storage.asm
@@ -31,6 +31,8 @@ disk_error:
     mov bx, DISK_ERROR
     call print
     call print_nl
+    mov dh, ah ; ah = error code, dl = disk drive that dropped the error
+    call print_hex ; check out the code at http://stanislavs.org/helppc/int_13-1.html
     jmp disk_loop
 
 sectors_error:
